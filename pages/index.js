006a42a32a627b437a805a2113e96e7c3f0434c3
Lovisa Berggren <id18lbn@cs.umu.se>
2023-02-27 11:40:56 +0100

    Added functionality to label text

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,60 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { supabase } from "./../lib/supabaseClient";
 import TextCard from "../components/TextCard";
 import buttonStyles from "@/styles/Button.module.css";
 
 function Page({ text, error }) {
+	const router = useRouter();
+
+	const setLabel = async (v) => {
+		try {
+			const { error } = await supabase.from("labels").insert([v]);
+
+			if (error) throw error;
+			alert("Label created successfully");
+			router.push("/");
+		} catch (error) {
+			alert(error.message);
+		}
+	};
+
+	const handleClick = (user_id, label) => {
+		const values = {
+			user_id,
+			text_id: text.id,
+			label,
+		};
+		setLabel(values);
+	};
+
 	return (
 		<>
 			<TextCard id={text.id} text={text.text} />
 			<div>
-				<button className={buttonStyles.button}>Positiv</button>
-				<button className={buttonStyles.button}>Neutral</button>
-				<button className={buttonStyles.button}>Negativ</button>
+				<button
+					className={buttonStyles.button}
+					onClick={() =>
+						handleClick(
+							"647ae96f-5c25-4918-8a21-52ebf7c08dbf",
+							"pos"
+						)
+					}
+				>
+					Positiv
+				</button>
+				<button
+					className={buttonStyles.button}
+					onClick={() => handleClick("neu")}
+				>
+					Neutral
+				</button>
+				<button
+					className={buttonStyles.button}
+					onClick={() => handleClick("neg")}
+				>
+					Negativ
+				</button>
 			</div>
 			<div>
 				<button className={buttonStyles.button}>Hoppa över</button>
