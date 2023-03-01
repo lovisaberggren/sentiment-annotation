9428b78b1c939bb09af2f82e23b0959dcc93404b
Anna Viklund <annamariaviklund@gmail.com>
2023-03-01 15:39:09 +0100

    TextCard.js -> TextCard.tsx

--- components/TextCard.tsx
+++ components/TextCard.tsx
@@ -1,6 +1,6 @@
 import textCardStyles from "../styles/TextCard.module.css";
 
-const TextCard = ({ id, text }) => {
+const TextCard = ({ text }: { text: string }) => {
 	return <div className={textCardStyles.card}>{text}</div>;
 };
 
