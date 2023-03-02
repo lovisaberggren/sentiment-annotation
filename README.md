71e4395cd5642ec6373c1030f389bd1ec3c0773c
Lovisa Berggren <id18lbn@cs.umu.se>
2023-03-02 10:44:01 +0100

    Fixed redirect url on login to depend on env

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Getting Started
+## Run locally
 
 Run `npm install` to install packages.
 
@@ -12,9 +12,14 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 ## Set up environment variables
 
-In the root directory, create `.env.local` file and add your Supabase URL and anon-key.
+In the root directory, create `.env.local` file and add your Supabase URL, anon-key and your site URL in production env.
 
 ```
 NEXT_PUBLIC_SUPABASE_URL=<your_supabase_url>
 NEXT_PUBLIC_SUPABASE_ANON_KEY=<your_supabase_anon_key>
+NEXT_PUBLIC_SITE_URL=<prod_env_url>
 ```
+
+## Set up Vercel
+
+Follow the [documentation](https://vercel.com/docs) on Vercel for setting up a project and linking it to your GitHub project.
--- components/Login.js
+++ components/Login.js
@@ -4,12 +4,24 @@ import { supabase } from "../lib/supabaseClient";
 export default function Login() {
 	const [email, setEmail] = useState("");
 
+	const getURL = () => {
+		let url =
+			process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+			process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+			"http://localhost:3000/";
+		// Make sure to include `https://` when not localhost.
+		url = url.includes("http") ? url : `https://${url}`;
+		// Make sure to including trailing `/`.
+		url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+		return url;
+	};
+
 	const handleLogin = async (email) => {
 		try {
 			const { data, error } = await supabase.auth.signInWithOtp({
 				email,
 				options: {
-					emailRedirectTo: "http://localhost:3000/",
+					emailRedirectTo: getURL,
 				},
 			});
 			if (error) throw error;
