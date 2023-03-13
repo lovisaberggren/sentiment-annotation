d1a9e1b85c08ec46df0a9619b7851ff4ca09627e
Lovisa Berggren <id18lbn@cs.umu.se>
2023-03-13 15:16:44 +0100

    Added mailto link to contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import styles from "@/styles/Contact.module.css";
 
 const Contact = () => {
 	return (
 		<>
 			<h2>Kontakt</h2>
 			<p>Har du några funderingar? Skicka iväg ett ett mejl till mig!</p>
-			<p>✉️ berggren.lovisa@gmail.com</p>
+			<p>
+				✉️
+				<a className={styles.mailto} href="mailto:berggren.lovisa@gmail.com">
+					berggren.lovisa@gmail.com
+				</a>
+			</p>
 		</>
 	);
 };
