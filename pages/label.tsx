80f65a4e834cd5a8e16e17d07260a554326a2801
Lovisa Berggren <id18lbn@cs.umu.se>
2023-03-15 15:24:42 +0100

    Moved reminders and scoreboard so that they are shown even if there are no texts left to label

--- pages/label.tsx
+++ pages/label.tsx
@@ -133,27 +133,19 @@ function Label({ session }) {
 									Hoppa över
 								</button>
 							</div>
-							{user ? (
-								<ScoreBoard scores={scoreBoard} user_id={user.id} />
-							) : (
-								<></>
-							)}
-							<h2>Kom ihåg!</h2>
-							<ul className={styles.remiderlist}>
-								<li>
-									Du kan klassificera hur många eller få texter du vill, och det
-									går alltid bra att komma tillbaka och fortsätta senare.
-								</li>
-								<li>Fokusera på vilken känsla skribenten har.</li>
-								<li>
-									Välj &quot;neutral&quot; för texter du tycker är neutrala.
-								</li>
-								<li>
-									Hoppa över om du är osäker eller om texten inte är lämplig.
-								</li>
-							</ul>
 						</>
 					)}
+					{user ? <ScoreBoard scores={scoreBoard} user_id={user.id} /> : <></>}
+					<h2>Kom ihåg!</h2>
+					<ul className={styles.remiderlist}>
+						<li>
+							Du kan klassificera hur många eller få texter du vill, och det går
+							alltid bra att komma tillbaka och fortsätta senare.
+						</li>
+						<li>Fokusera på vilken känsla skribenten har.</li>
+						<li>Välj &quot;neutral&quot; för texter du tycker är neutrala.</li>
+						<li>Hoppa över om du är osäker eller om texten inte är lämplig.</li>
+					</ul>
 				</>
 			)}
 		</>
