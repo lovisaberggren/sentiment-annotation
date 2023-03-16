bb453196b7a13cef92ef4786e055fe86319c88b0
Lovisa Berggren <id18lbn@cs.umu.se>
2023-03-16 11:21:55 +0100

    Changed favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="sv">
-			<Head />
+			<Head>
+				<link rel="shortcut icon" href="/images/favicon-01.png" />
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/images/favicon-01.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/images/favicon-02.png"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
