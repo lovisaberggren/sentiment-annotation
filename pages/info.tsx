8f849c2dbf9a783622f7e09be3ea5c7b1421927f
Lovisa Berggren <id18lbn@cs.umu.se>
2023-03-20 10:07:26 +0100

    Fixed so that user is redirected to /login if there is no session on /info or /label

--- pages/info.tsx
+++ pages/info.tsx
@@ -11,7 +11,7 @@ export default function Info({ session }) {
 
 	useEffect(() => {
 		if (!session) {
-			router.push("/");
+			router.push("/login");
 		}
 	}, [session]);
 
--- pages/label.tsx
+++ pages/label.tsx
@@ -35,7 +35,7 @@ function Label({ session }) {
 
 	useEffect(() => {
 		if (!session) {
-			router.push("/start");
+			router.push("/login");
 		}
 		if (session?.user) {
 			setUser(session.user);
